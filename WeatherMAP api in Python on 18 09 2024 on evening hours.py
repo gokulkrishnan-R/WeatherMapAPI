dde08290474efbe5b8c5c74f9faa848f60ffc179
gokulkrishnan-R~ <gokulkrish981997@gmail.com>
2024-09-18 22:02:37 +0100

    Second Commit

--- WeatherMAP api in Python on 18 09 2024 on evening hours.py
+++ WeatherMAP api in Python on 18 09 2024 on evening hours.py
@@ -4,10 +4,11 @@
 
 import json
 import requests as request
+import csv
 
 class WeatherMap():
 
-	def __init__(self,city_name,API_KEY,BASE_URL,url,response,data,main,description,current_temperature,pressure,humidity,co_ordinates):
+	def __init__(self,city_name,API_KEY,BASE_URL,url,response,data,main,description,current_temperature,pressure,humidity,co_ordinates,sno):
 		print("\n")
 		self.city_name=str(input("Enter the city name:"))
 		self.API_KEY=API_KEY
@@ -21,6 +22,7 @@ class WeatherMap():
 		self.pressure=pressure
 		self.humidity=humidity
 		self.co_ordinates=co_ordinates
+		self.sno=sno
 
 	def get_city_name(self):
 		#Creating urls from base urls
@@ -47,6 +49,30 @@ class WeatherMap():
 			print(f"Pressure:{self.pressure}\n")
 			print(f"Humidity:{self.humidity}\n")
 			print(f"Co-Ordinates:{self.co_ordinates}\n")
+			print("\n")
+
+			#Saving the datas in the text_file
+			with open("weather_datas.txt","a") as f:
+				self.sno=0
+				f.write(f"S.No:{self.sno}\n")
+				f.write(f"City_name:{self.city_name}\n")
+				f.write(f"Weather_Description:{self.weather_description}\n")
+				f.write(f"Current_Temperature:{self.current_temperature}\n")
+				f.write(f"Pressure:{self.pressure}\n")
+				f.write(f"Humidity:{self.humidity}\n")
+				f.write(f"Co_Ordinates:{self.co_ordinates}\n\n\n")
+				print("\n")
+				print("{Weather details are copied in the text file successfully!")
+				self.sno=self.sno+1
+
+			#Saving the datas in the CSV format
+			with open("weather_datas.csv",mode="a+") as f:
+				fieldnames=["City_name","Weather_Description","Current_Temperature","Pressure","Humidity","Co_Ordinates"]	
+				writer=csv.DictWriter(f,fieldnames)
+				writer.writeheader()
+				writer.writerow({"City_name":self.city_name,"Weather_Description":self.weather_description,"Current_Temperature":self.current_temperature,"Pressure":self.pressure,"Humidity":self.humidity,"Co_Ordinates":self.co_ordinates})
+				print("CSV file created and copied datas successfully!}")
+				print("\n")
 
 		elif self.response.status_code == 429:
 			print(f"Message:{self.response.status_code} Too many requests on these API's. Hence Blocked. Try again later!")
@@ -57,6 +83,7 @@ class WeatherMap():
 		else:
 			print(f"City {self.city_name} is not found! Please check the spelling before you type in!")
 
+
 # OpenWeatherMap API Key
 API_KEY="a6e1bc1ceba3ef6ae534d88ce956b0cc"
 
@@ -64,5 +91,5 @@ API_KEY="a6e1bc1ceba3ef6ae534d88ce956b0cc"
 BASE_URL="https://api.openweathermap.org/data/2.5/weather"
 
 #datas instance for class WeatherMap()
-details=WeatherMap("",API_KEY,BASE_URL,"","","","","","","","","")
+details=WeatherMap("",API_KEY,BASE_URL,"","","","","","","","","","")
 print(details.get_city_name())
